23da76677733e9f0fe40ee1d78897e3e589144aa
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2021-11-30 23:37:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def object_detection():
 if __name__=="__main__":
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0',debug=True, port=port)
-    app.run(debuf=True)
+    app.run(debug=True)
 
 
